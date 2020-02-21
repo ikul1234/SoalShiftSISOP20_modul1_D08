0639892cc9dcbda64ae714b59f23d2f4e0f6da97
ikul1234 <37994966+ikul1234@users.noreply.github.com>
2020-02-21 13:04:38 +0700

    Delete soal1b.sh

--- soal1b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-var=`cat test.txt`
-
-awk -F '	' -v region="$var" 'FNR > 1 {if ($13==region) {a[$11] += $21}} END {for (i in a) {print a[i],i}}' Sample-Superstore.tsv | sort -g | awk 'NR <=2 {printf "%s ", $2}' > test2.txt
