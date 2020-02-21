b770fb856d87ddc455c755e29ae33f3c94e13498
ikul1234 <37994966+ikul1234@users.noreply.github.com>
2020-02-21 13:04:51 +0700

    Delete soal1c.sh

--- soal1c.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-var=`cat test.txt`
-var2=`cat test2.txt`
-
-IFS=" " read -r -a array <<< "$var2"
-
-awk -F '	' -v region="$var" -v state1="${array[0]}" -v state2="${array[1]}" 'FNR > 1 {if ($13==region && ($11==state1 || $11==state2)) {a[$17] += $21}} END {for (i in a) {print a[i],i}}' Sample-Superstore.tsv | sort -g | awk 'NR <=10  {print $0}'
